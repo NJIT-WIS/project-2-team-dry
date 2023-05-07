764798232f4b7eab5a5a61bbbb5354125dcde3db
roopali <rs366@njit.edu>
2023-05-07 10:23:09 -0400

    testfix3

--- pages/Contact.js
+++ pages/Contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import styles from '../styles/contact.module.css'
 import Layout, { siteTitle } from '../components/layout'
-import SubscribeContainer from '../components/subscribe'
+import SubscribeContainer from '../components/Subscribe'
 
 export default function Contact() {
   return (
