3cb25e899602c8a5bb768e7ece716cff22d0bea6
roopali <rs366@njit.edu>
2023-05-07 12:49:07 -0400

    added gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,29 @@
 import '../styles/global.css'
 import Footer from '../components/Footer'
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+
+// const dotenv = require('dotenv');
+// dotenv.config();
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = url => {
+      window.gtag('config', process.env.GOOGLE_ANALYTICS_GTAG, {
+        page_path: url,
+      });
+    }
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    }
+  }, [router.events]);
+
   return (
     <>
+    
+    
   <Component {...pageProps} />
   <Footer/>
   </>
