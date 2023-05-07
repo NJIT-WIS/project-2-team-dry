90344b307c8029e57c6f43fd2fc271a5566bf3a7
Yatindra Mahadik <mahadik.yatindra@gmail.com>
2023-05-07 16:43:13 -0400

    refactored the code

--- pages/About.js
+++ pages/About.js
@@ -3,7 +3,6 @@ import Head from 'next/head'
 import utilStyles from '../styles/utils.module.css'
 import Layout, { siteTitle } from '../components/layout'
 
-
 const About = () => {
   const teamMembers = [
     {
